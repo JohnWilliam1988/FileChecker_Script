f78b0da05c633d9ad9486722bd8f0bd9553669ad
john <johnwilliam@126.com>
2019-12-19 02:08:54 +0800

    修改为可清晰显示切割走线图的脚本

--- DrawPlt.py
+++ DrawPlt.py
@@ -8,9 +8,7 @@ print(sys.getdefaultencoding())
 allPltFiles = []
 #plt文件根目录
 pltFileRootPath = "plt/"
-#存储当前Plt文件坐标List
-Coordinates_X = []
-Coordinates_Y = []
+
 
 
 def FindAllPltFilePath(path):
@@ -20,56 +18,65 @@ def FindAllPltFilePath(path):
             if os.path.splitext(file_name)[-1] == '.plt':
                 print(os.path.join(path, file_name))
                 allPltFiles.append(os.path.join(path, file_name))
-    print(len(allPltFiles))
+    print("总共PLT文件为：%d"  %len(allPltFiles))
             
 
 def CaculateCurrentPltCoordinates(filePath):
+    #存储当前Plt文件坐标List
+    Coordinates_X = []
+    Coordinates_Y = []
     #Mac格式
     file = open(filePath, encoding='utf-8')
     print("文件路径为： " + file.name)
-    tempPath = os.path.basename(filePath)
-    fileName = os.path.splitext(tempPath)[0]
+    # tempPath = os.path.basename(filePath)
+    # fileName = os.path.splitext(tempPath)[0]
     contents = file.read()
-    #print(contents)
+    print(contents)
     for coordinate in contents.split():
         #print(coordinate)
         if (coordinate[0] == 'D'):
             coordinate = coordinate.strip('D')
-            #print(coordinate)
+            print(coordinate)
             coordinate = coordinate.split(',', 1)
-            #print(coordinate[0])
-            #print(coordinate[1])
+            print(coordinate[0])
+            print(coordinate[1])
             Coordinates_X.append(int(coordinate[0]))
             Coordinates_Y.append(int(coordinate[1]))
-    return fileName
-
+    return Coordinates_X, Coordinates_Y
+   
 
 def SaveCoordinatesJPG(coordsX, coordsY, filename):
-    plt.scatter(coordsX, coordsY, s = 2)
-
-    # 设置图表标题并给坐标轴加上标签
-    plt.title(filename, fontsize = 24)
     plt.xlabel('X', fontsize = 14)
     plt.ylabel('Y', fontsize = 14)
 
     # 设置刻度标记的大小
     plt.tick_params(axis='both', which='major', labelsize=14)
 
-    plt.plot(coordsX, coordsY, 'ob')
     # 设置每个坐标轴的取值范围
-    plt.axis([0, 10000, 0, 10000]) 
+    plt.axis([0, 8000, 0, 6000]) 
     plt.legend(loc = 'best')    # 设置 图例所在的位置 使用推荐位置
+    # 设置图表标题并给坐标轴加上标签
+    plt.title(filename, fontsize = 24)
+    plt.scatter(coordsY, coordsX, s = 3)
+    plt.plot(coordsY, coordsX, color="r", linestyle="--", marker="*", linewidth=3.0)
     #plt.show() 
     #保存图象
-    plt.savefig('SavdJpg/' + filename + '.jpg')
-    print("保存图片") 
+    plt.savefig('ScreenProtectorJPG/' + filename + '.jpg')
+    print("保存图片成功！！！") 
     plt.close()
-    print("plt.close()")
     return
 
 
 if __name__ == "__main__":
     FindAllPltFilePath(pltFileRootPath)
+    i = 0
     for file in allPltFiles:
-        currentPlt_name = CaculateCurrentPltCoordinates(file)
-        SaveCoordinatesJPG(Coordinates_X, Coordinates_Y, currentPlt_name)
+        tempPath = os.path.basename(file)
+        fileName = os.path.splitext(tempPath)[0]
+        (coordinateX, coordinateY) = CaculateCurrentPltCoordinates(file)
+        SaveCoordinatesJPG(coordinateX, coordinateY, fileName)
+        coordinateX.clear()
+        coordinateY.clear()
+        print("保存了%d个jpg！！！！！" %i)
+        i += 1
+    print("所有Plt绘制完成！！！！！")
