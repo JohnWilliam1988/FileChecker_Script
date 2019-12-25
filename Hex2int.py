14d1a053b3004a9a4ed05a40d9366311154d2346
zhoujunliang <zhoujunliang@3dinlife.com>
2019-12-25 20:28:00 +0800

    修改通过Shell调用

--- Hex2int.py
+++ Hex2int.py
@@ -1,22 +1,34 @@
 import os
+import sys
 
-#Source Hex Plt Flie
-file = open(r'Nintendo WiiU.txt', encoding='utf-8')
-contents = file.read()
+def ConventHex2IntPlt(file_Path):
+    #Source Hex Plt Flie
+    Sourcefile = open(file_Path, encoding='utf-8')
+    contents = Sourcefile.read()
+    (filepath, tempfilename) = os.path.split(file_Path)
+    (filename, extension) = os.path.splitext(tempfilename)
+    print('filepath is : ' + filepath)
+    print('filename is : ' + filename)
+    print('extension is : ' + extension)
 
-file2Write = None
-try:
-    file2Write = open(r'Nintendo WiiU.plt', 'w', encoding='utf-8')
-except IOError:
-    msg = 'Unable to create file on disk.'
-    file2Write.close()
-    print(msg)
-finally:
-    for content in contents.split():
-        print(content)
-        convent = int(content, 16)
-        print(chr(convent))
-        file2Write.write(chr(convent))
-    file2Write.close()
+    writeFilePath = os.path.join(filepath, filename + '.plt')
+    print('writeFilePath is :' + writeFilePath)
+    file2Write = None
+    try:
+        file2Write = open(writeFilePath, 'w', encoding='utf-8')
+    except IOError:
+        msg = 'Unable to create file on disk.'
+        file2Write.close()
+        print(msg)
+    finally:
+        for content in contents.split():
+            #print(content)
+            convent = int(content, 16)
+            #print(chr(convent))
+            file2Write.write(chr(convent))
+        file2Write.close()
+        print('Convent Done!!!')
+    return
 
-
+if __name__ == "__main__":
+    ConventHex2IntPlt(sys.argv[1])
