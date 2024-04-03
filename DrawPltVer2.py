0b766a05c861338ee998fbbb52687c1975c8144c
John <johnwilliam@126.com>
2024-04-03 16:02:59 +0800

    绘图机跟切膜机共用脚本，根据参数显示提刀路径，以及根据参数调整显示原点位置

--- DrawPltVer2.py
+++ DrawPltVer2.py
@@ -6,7 +6,8 @@ import matplotlib; matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_pdf import PdfPages
 print(sys.getdefaultencoding())
-
+#中文显示
+plt.rcParams['font.sans-serif'] = ['Arial Unicode MS']
 
 font = {'family': 'sans-serif',
         'color':  'blue',
@@ -15,16 +16,33 @@ font = {'family': 'sans-serif',
         }
 
 
-def Draw(filePath):
+#@filePath plt路径
+#@bPlotUp 是否绘制提刀路径
+#@原点位置0,1,2,3 顺时针对应左上、右上、右下、左下四个位置
+def Draw(filePath, bPlotUp = True, originPos = 0):
     plt.figure(num = 1, figsize = (15, 15), dpi = 100)
     ax = plt.gca()
-    #x轴方向调整：
-    ax.xaxis.set_ticks_position('top') #将x轴的位置设置在顶部
-    # ax.invert_xaxis() #x轴反向
-    
-    #y轴方向调整：
-    # ax.yaxis.set_ticks_position('left') #将y轴的位置设置在右边
-    ax.invert_yaxis() #y轴反向
+    title = ''
+    if (originPos == 0):
+        ax.xaxis.set_ticks_position('top')
+        ax.yaxis.set_ticks_position('left')
+        ax.invert_yaxis() 
+        title = 'origin pos top-left'
+    elif (originPos == 1):
+        ax.xaxis.set_ticks_position('top')
+        ax.yaxis.set_ticks_position('right') 
+        ax.invert_xaxis()
+        ax.invert_yaxis() 
+        title = 'origin pos top-right'
+    elif (originPos == 2):
+        ax.xaxis.set_ticks_position('bottom') 
+        ax.yaxis.set_ticks_position('right') 
+        ax.invert_xaxis() 
+        title = 'origin pos bottom-right'
+    elif (originPos == 3):
+        ax.xaxis.set_ticks_position('bottom')
+        ax.yaxis.set_ticks_position('left')
+        title = 'origin pos bottom-left'
 
     #Mac格式
     file = open(filePath, encoding='utf-8')
@@ -34,7 +52,8 @@ def Draw(filePath):
     contents = file.read()
 
     bDown = False
-    minX, minY, maxX, maxY = 0, 0, 0, 0
+    #因为这里需要确保是D里面的坐标数据
+    minX, minY, maxX, maxY = 10000, 10000, -1000, -1000
     pointX = []
     pointY = []
     for coordinate in contents.split():
@@ -45,6 +64,7 @@ def Draw(filePath):
             pointX.append(int(coordinate[0]))
             pointY.append(int(coordinate[1]))
             bDown = False
+
         elif (coordinate[0] == 'D'):
             coordinate = coordinate.strip('D')
             coordinate = coordinate.split(',', 1)
@@ -52,25 +72,28 @@ def Draw(filePath):
             pointY.append(int(coordinate[1]))
             bDown = True
 
-            if (int(coordinate[0]) < minX):
+            #获取最大最小值，用于计算宽高
+            if (minX > int(coordinate[0])):
                 minX = int(coordinate[0])
-            if (int(coordinate[0]) > maxX):
+            if (maxX < int(coordinate[0])):
                 maxX = int(coordinate[0])
-            if (int(coordinate[1]) < minY):
+            if (minY > int(coordinate[1])):
                 minY = int(coordinate[1])
-            if (int(coordinate[1]) > maxY):
+            if (maxY < int(coordinate[1])):
                 maxY = int(coordinate[1])
+
         else:
             continue
 
         #有两个点才能进行绘制
-        if len(pointX) > 1 :       
-            #绘制提刀运动轨迹
+        if len(pointX) > 1 :      
+            #绘制切割运动轨迹 
             if bDown:
                 plt.plot(pointY, pointX, color = "r", linewidth = 1)
-            #绘制切割运动轨迹
+            #绘制提刀运动轨迹
             else:
-                plt.plot(pointY, pointX, linestyle = 'dotted', color = "b", linewidth = 1)
+                if bPlotUp:
+                    plt.plot(pointY, pointX, linestyle = 'dashdot', color = "b", linewidth = 0.25)
 
             #删除前一个点
             del pointX[0]
@@ -79,9 +102,9 @@ def Draw(filePath):
             continue
        
     
-      # 设置图表标题并给坐标轴加上标签
-    plt.title("V2", fontsize = 18)
-
+    # 设置图表标题并给坐标轴加上标签
+    plt.title(title, fontsize = 18)
+    print(minX, maxX, minY, maxY)
     plt.xlabel(str((maxY - minY) / 40) + "mm", font)
     plt.ylabel(str((maxX - minX) / 40) + "mm", font)
 
@@ -100,4 +123,4 @@ def Draw(filePath):
 
 if __name__ == "__main__":
     filePath = '/Users/zhoujunliang/Downloads/aaa.plt'
-    Draw(filePath)
+    Draw(filePath, True, 0)
